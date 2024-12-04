f631febfc145a4aeb7d6953cc94c4c6ec778a8f3
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2024-12-04 16:38:56 +0100

    debut js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,4 @@
-// *** LIGHTBOX ***
+// *** LIGHTBOX ***//
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('ul img');
+    let expandedImage = null;
